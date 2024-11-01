cc08a488c74cf9199d11d6be8e7bad3bee34f8e1
Murat Aybars <39916128+aybarsm@users.noreply.github.com>
2024-11-01 17:42:36 +0000

    gitignore update, remove storage, resources/view gitkeep

--- config/filesystems.php
+++ config/filesystems.php
@@ -1,5 +1,6 @@
 <?php
 
+
 return [
     'default' => 'local',
     'disks' => [
--- config/view.php
+++ config/view.php
@@ -4,8 +4,5 @@ return [
     'paths' => [
         resource_path('views'),
     ],
-    'compiled' => env(
-        'VIEW_COMPILED_PATH',
-        realpath(storage_path('cache/views'))
-    ),
+    'compiled' => env('VIEW_COMPILED_PATH', realpath(storage_path('cache/views'))),
 ];
