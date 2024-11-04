183aaee264d6ca917c144d75ccf7a6530365c524
Murat Aybars <39916128+aybarsm@users.noreply.github.com>
2024-11-04 14:14:46 +0000

    Helper buildProcessArgs

--- tests/Unit/HelperProcessTest.php
+++ tests/Unit/HelperProcessTest.php
@@ -3,11 +3,15 @@
 use App\Services\Helper;
 
 describe('Helper Process', function () {
+    $expected['defaults'] = [
+        '--working-dir=path/to/default-working-dir',
+    ];
     $expected['args'] = [
         '--working-dir=path/to/working-dir',
         '--config=path/to/config.json',
         '--debug',
     ];
+//    $expected['full'] = $expected['defaults']
 
     it('process command args when args is list', function () use ($expected) {
         $args = ['working-dir=path/to/working-dir', '--config=path/to/config.json', '-debug'];
