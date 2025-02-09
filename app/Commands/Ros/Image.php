7100a4cef18953caeaf3a27f5cf559a85a5a77b6
Murat Aybars <39916128+aybarsm@users.noreply.github.com>
2025-02-09 13:50:20 +0000

    ros:image

--- app/Commands/Ros/Image.php
+++ app/Commands/Ros/Image.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace App\Commands\Ros;
 
 use App\Framework\Commands\Command;
+use Illuminate\Support\Arr;
 use Symfony\Component\Finder\Finder;
+use Symfony\Component\Finder\SplFileInfo;
 
 class Image extends Command
 {
@@ -14,10 +16,27 @@ class Image extends Command
 
     public function handle()
     {
-        $keyrings = iterator_to_array(Finder::create()
+
+
+        $copy['exact'] = [
+            '/etc/apt/sources.list.d/debian.sources'
+        ];
+
+        array_merge($copy['exact'], $this->splIteratorToArray(Finder::create()
             ->in('/usr/share/keyrings')
             ->notName('-buster-')
-            ->notName('-bullseye-'));
-        dump($keyrings);
+            ->notName('-bullseye-')));
+
+        array_merge($copy['exact'], $this->splIteratorToArray(Finder::create()
+            ->in('/etc/apt/trusted.gpg.d')
+            ->notName('-buster-')
+            ->notName('-bullseye-')));
+
+        dump($copy['exact']);
+    }
+
+    protected function splIteratorToArray($finder)
+    {
+        return Arr::map(iterator_to_array($finder), fn (SplFileInfo $file) => $file->getRealPath());
     }
 }
